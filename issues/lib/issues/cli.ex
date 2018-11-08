3e2c522740ee93887728575093ddc06a15fa4ca6
va@no.where.com <va@no.where.com>
2018-11-08 15:51:29 +0200

    cli

--- issues/lib/issues/cli.ex
+++ issues/lib/issues/cli.ex
@@ -18,6 +18,21 @@ end
 
 def process({user, project, _count}) do
   Issues.GithubIssues.fetch(user, project)
+  |> decode_response()
+  |> sort_into_descending_order()
+end
+
+def sort_into_descending_order(list_of_issues) do
+  list_of_issues
+  |> Enum.sort(fn issue_1, issue_2 ->
+                issue_1["created_at"] >= issue_2["created_at"]
+              end)
+end
+
+def decode_response({:ok, body}), do: body
+def decode_response({:error, error}) do
+  IO.puts "Error fetching from GH: #{error["message"]}"
+  System.halt(2)
 end
 
 @doc """
@@ -45,6 +60,5 @@ end
 def args_to_internal_representation(_) do
   :help
 end
-
 
 end
--- issues/test/cli_test.exs
+++ issues/test/cli_test.exs
@@ -1,7 +1,8 @@
 defmodule CliTest do
   use ExUnit.Case
   doctest Issues
-  import Issues.CLI, only: [parse_args: 1]
+  import Issues.CLI, only: [parse_args: 1,
+                            sort_into_descending_order: 1]
 
   test ":help ret by option parsing with -h and --help options" do
     assert parse_args(["-h", "anything"]) == :help
@@ -16,4 +17,12 @@ defmodule CliTest do
     assert parse_args(["user", "project"]) == {"user", "project", 4}
   end
 
+  test "sort_into_descending_order" do
+    res = sort_into_descending_order(fake_list(["c", "a", "b"]))
+    issues = for issue <- res, do: Map.get(issue, "created_at")
+    assert issues == ~w{ c b a }
+  end
+  defp fake_list(values) do
+    for val <- values, do: %{"created_at" => val, "other_data" => "xxx"}
+  end
 end
