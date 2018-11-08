2f031e67a1ae290eb1f9d8bf2deb91b16524c5ec
va@no.where.com <va@no.where.com>
2018-11-08 14:03:39 +0200

    cli

--- issues/lib/issues/cli.ex
+++ issues/lib/issues/cli.ex
@@ -6,8 +6,20 @@ the various functions that end up generating a
 table of the last _n_ issues in a github project
 """
 def run(argv) do
-  parse_args(argv)
+  argv
+  |> parse_args
+  |> process
 end
+
+
+def process(:help) do
+  IO.puts "usage: issues <user> <project> [ count | #{@default_count} ]"
+end
+
+def process({user, project, _count}) do
+  Issues.GithubIssues.fetch(user, project)
+end
+
 @doc """
 `argv` can be -h or --help, which returns :help.
 Otherwise it is a github user name, project name, and (optionally)
@@ -15,17 +27,24 @@ the number of entries to format.
 Return a tuple of `{ user, project, count }`, or `:help` if help was given.
 """
 def parse_args(argv) do
-  parse = OptionParser.parse(argv, switches: [ help: :boolean],
-  aliases: [ h: :help ])
-  case parse do
-  { [ help: true ], _, _ }
-  -> :help
-  { _, [ user, project, count ], _ }
-  -> { user, project, String.to_integer(count) }
-  { _, [ user, project ], _ }
-  -> { user, project, @default_count }
-  _ -> :help
-  end
+  parse = OptionParser.parse(argv,
+                             switches: [help: :boolean],
+                             aliases: [ h: :help ])
+  |> elem(1)
+  |> args_to_internal_representation()
 end
+
+def args_to_internal_representation([user, project, count]) do
+  { user, project, String.to_integer(count) }
+end
+
+def args_to_internal_representation([user, project]) do
+  { user, project, @default_count }
+end
+
+def args_to_internal_representation(_) do
+  :help
+end
+
 
 end
